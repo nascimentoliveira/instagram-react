7f66b97a476dfd455b5ae8e305b74e52cf39eea9
Thiago Nascimento <nascimentoliveira@gmail.com>
2022-09-17 20:08:29 -0300

    refactor (Stories): componentization

--- src/Stories.js
+++ src/Stories.js
@@ -1,82 +1,40 @@
-export default function Stories() {
+function Story(props) {
   return (
+    <div class="story">
+      <div class="imagem">
+        <img src={props.img} />
+      </div>
+      <div class="usuario">
+        {props.usuario}
+      </div>
+    </div >
+  );
+}
 
+function Setinha() {
+  return (
+    <div class="setinha">
+      <ion-icon name="chevron-forward-circle"></ion-icon>
+    </div>
+  );
+}
+
+export default function Stories() {
+  const stories = [
+    { img: "assets/img/9gag.svg", usuario: "9gag" },
+    { img: "assets/img/meowed.svg", usuario: "meowed" },
+    { img: "assets/img/barked.svg", usuario: "barked" },
+    { img: "assets/img/nathanwpylestrangeplanet.svg", usuario: "nathanwpylestrangeplanet" },
+    { img: "assets/img/wawawicomics.svg", usuario: "wawawicomics" },
+    { img: "assets/img/respondeai.svg", usuario: "respondeai" },
+    { img: "assets/img/filomoderna.svg", usuario: "filomoderna" },
+    { img: "assets/img/memeriagourmet.svg", usuario: "memeriagourmet" }
+  ];
+
+  return (
     <div class="stories">
-      <div class="story">
-        <div class="imagem">
-          <img src="assets/img/9gag.svg" />
-        </div>
-        <div class="usuario">
-          9gag
-        </div>
-      </div>
-
-      <div class="story">
-        <div class="imagem">
-          <img src="assets/img/meowed.svg" />
-        </div>
-        <div class="usuario">
-          meowed
-        </div>
-      </div>
-
-      <div class="story">
-        <div class="imagem">
-          <img src="assets/img/barked.svg" />
-        </div>
-        <div class="usuario">
-          barked
-        </div>
-      </div>
-
-      <div class="story">
-        <div class="imagem">
-          <img src="assets/img/nathanwpylestrangeplanet.svg" />
-        </div>
-        <div class="usuario">
-          nathanwpylestrangeplanet
-        </div>
-      </div>
-
-      <div class="story">
-        <div class="imagem">
-          <img src="assets/img/wawawicomics.svg" />
-        </div>
-        <div class="usuario">
-          wawawicomics
-        </div>
-      </div>
-
-      <div class="story">
-        <div class="imagem">
-          <img src="assets/img/respondeai.svg" />
-        </div>
-        <div class="usuario">
-          respondeai
-        </div>
-      </div>
-
-      <div class="story">
-        <div class="imagem">
-          <img src="assets/img/filomoderna.svg" />
-        </div>
-        <div class="usuario">
-          filomoderna
-        </div>
-      </div>
-
-      <div class="story">
-        <div class="imagem">
-          <img src="assets/img/memeriagourmet.svg" />
-        </div>
-        <div class="usuario">
-          memeriagourmet
-        </div>
-      </div>
-
-      <div class="setinha">
-        <ion-icon name="chevron-forward-circle"></ion-icon>
-      </div>
+      {stories.map(story => <Story img={story.img} usuario={story.usuario} />)}
+      <Setinha />
     </div>
   );
 }
